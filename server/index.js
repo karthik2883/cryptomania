64f04a2c8a7b136a91c8077f627057797eac1fad
karthik_uandme <karthik.b@uandme.org>
2018-01-29 17:04:19 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const process = require("process");
+const url = require('url');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const engine = require('consolidate');
+
+app.use(express.static(path.join(__dirname, 'view')));
+app.set('views', __dirname + '/view');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+//generic functionality with api docs
+//
+app.get('/check', function (req, res) {
+    res.send('welcome to cryptomania');
+});
+
+process.setMaxListeners(0);
+const server = http.createServer(app);
+server.listen(8000);
+ 
